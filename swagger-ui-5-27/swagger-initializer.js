175e5aa5310f66a768b1c85bf4c465e2c18a5443
morfi <moravcik@exalogic.sk>
2025-08-26 15:42:06 +0200

    Aktualizácia oberon web api g1

--- swagger-ui-5-27/swagger-initializer.js
+++ swagger-ui-5-27/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
     
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "http://localhost:8084/swagger_oberon_g1.json",
+    url: "https://raw.githubusercontent.com/morfirk/oberon_web_api/refs/heads/main/openapi_oberon_web_gen1.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
